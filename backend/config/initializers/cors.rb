8d2c3bd10a10290fa739869d1f36a9165eac3dd0
br1ansouza <briandesouza1597@gmail.com>
2026-09-05 22:57:38 -0300

    feat: protege a api com cors e rate limiting

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,16 +1,15 @@
-# Be sure to restart your server when you modify this file.
+LOCAL_ORIGINS = %r{\Ahttp://(localhost|127\.0\.0\.1|\d+\.\d+\.\d+\.\d+)(:\d+)?\z}
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
+configured_origins = ENV.fetch("FRONTEND_URL", "").split(",").map { |origin| origin.strip.chomp("/") }.reject(&:empty?)
+allowed_origins = Rails.env.production? ? configured_origins : configured_origins + [ LOCAL_ORIGINS ]
 
-# Read more: https://github.com/cyu/rack-cors
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins(*allowed_origins)
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+    resource "*",
+      headers: :any,
+      methods: [ :get, :post, :patch, :put, :delete, :options, :head ],
+      max_age: 600
+  end
+end
